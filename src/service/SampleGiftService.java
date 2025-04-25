424581041ba6a19e4d0a0b65450e4cfd337215ef
liyusang1 <liyusang1@naver.com>
2025-04-25 11:00:48 +0900

    feat : GiftSampleService 추가

---
+++ src/service/SampleGiftService.java
@@ -0,0 +1,55 @@
+package service;
+
+import dao.SampleGiftDao;
+import model.SampleGift;
+
+import java.util.List;
+
+public class SampleGiftService {
+    private final SampleGiftDao sampleGiftDao;
+
+    public SampleGiftService() {
+        this.sampleGiftDao = new SampleGiftDao();
+    }
+
+    /**
+     * 상품 업데이트를 처리
+     *
+     * @param name   상품 이름
+     * @param gStart 시작 값
+     * @param gEnd   끝 값
+     * @param gno    상품 번호
+     */
+    public int updateGift(String name, int gStart, int gEnd, int gno) {
+        // 비즈니스 로직을 이곳에서 추가할 수 있습니다.
+        // 예: 유효성 검사, 값 체크 등
+        return sampleGiftDao.updateGift(name, gStart, gEnd, gno);
+    }
+
+    /**
+     * 상품 생성을 처리
+     *
+     * @param sampleGift 상품 이름
+     */
+    public int insertGift(SampleGift sampleGift) {
+        // 비즈니스 로직을 이곳에서 추가할 수 있습니다.
+        // 예: 유효성 검사, 값 체크 등
+        if (sampleGift.getName() == null || sampleGift.getName().isBlank()) {
+            System.out.println("❌ 상품 이름이 비어있습니다.");
+            return 0;
+        }
+
+        return sampleGiftDao.insertGift(sampleGift);
+    }
+
+    /**
+     * 상품 조회를 처리
+     *
+     * @param
+     */
+    public List<SampleGift> getAllGifts() {
+        // 비즈니스 로직을 이곳에서 추가할 수 있습니다.
+        // 예: 유효성 검사, 값 체크 등
+        return sampleGiftDao.getAllGifts();
+    }
+}
