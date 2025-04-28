2e5d6f91db34ac044261b2deaed67eb418d88174
liyusang1 <liyusang1@naver.com>
2025-04-28 10:36:40 +0900

    refactor : lombok 적용

--- src/main/java/model/dto/SampleGiftDto.java
+++ src/main/java/model/dto/SampleGiftDto.java
@@ -1,5 +1,8 @@
 package model.dto;
 
+import lombok.Data;
+
+@Data
 public class SampleGiftDto {
     private int gno;
     private String name;
@@ -13,38 +16,6 @@ public class SampleGiftDto {
         this.gno = gno;
         this.name = name;
         this.g_start = g_start;
-        this.g_end = g_end;
-    }
-
-    public int getGno() {
-        return gno;
-    }
-
-    public void setGno(int gno) {
-        this.gno = gno;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getG_start() {
-        return g_start;
-    }
-
-    public void setG_start(int g_start) {
-        this.g_start = g_start;
-    }
-
-    public int getG_end() {
-        return g_end;
-    }
-
-    public void setG_end(int g_end) {
         this.g_end = g_end;
     }
 }
