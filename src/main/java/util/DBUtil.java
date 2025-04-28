c49e7c440f8d6fee43738b04574538483333ed67
liyusang1 <liyusang1@naver.com>
2025-04-28 09:33:37 +0900

    refactor : DB Connection secret값 환경변수 -> dotenv로 변경

--- src/main/java/util/DBUtil.java
+++ src/main/java/util/DBUtil.java
@@ -1,10 +1,16 @@
 package util;
-import java.sql.*;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBUtil {
-    static String URL = System.getenv("DB_HOST");
-    static String DB_USER = System.getenv("DB_NAME");
-    static String DB_PASSWORD = System.getenv("DB_PW");
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String URL = dotenv.get("DB_HOST");
+    private static final String DB_USER = dotenv.get("DB_NAME");
+    private static final String DB_PASSWORD = dotenv.get("DB_PW");
 
     // DB 연결 메서드
     public static Connection getConnection() throws SQLException {
