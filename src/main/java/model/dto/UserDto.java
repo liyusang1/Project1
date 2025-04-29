34ac9cf9d268c7bb345ecba8ae6851a54e6adeef
liyusang1 <liyusang1@naver.com>
2025-04-29 14:11:39 +0900

    feat : LendingDto, BookDto 수정 및 UserDto 추가

---
+++ src/main/java/model/dto/UserDto.java
@@ -0,0 +1,31 @@
+package model.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class UserDto {
+    private Long userId;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String membershipType; //0:일반유저 , 1:관리자
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String password;
+
+    public UserDto() {
+    }
+
+    public UserDto(Long userId, String name, String email, String phoneNumber, String membershipType, LocalDateTime createdAt, LocalDateTime updatedAt, String password) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.membershipType = membershipType;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.password = password;
+    }
+}
