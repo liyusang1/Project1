263927c4eda56d32e271704a6db883846a44edb3
ghyunjin0913 <ghyunjin0913@gmail.com>
2025-04-30 16:22:36 +0900

    feat: 존재하는 도서 신청 불가 기능 추가

--- src/main/java/service/RequestBookService.java
+++ src/main/java/service/RequestBookService.java
@@ -1,6 +1,7 @@
 package service;
 
 import model.dao.RequestBookDao;
+import model.dto.BookDto;
 import model.dto.RequestsDto;
 
 import java.sql.Date;
@@ -11,7 +12,15 @@ public class RequestBookService {
 
     public RequestBookService() { this.requestBookDao = new RequestBookDao(); }
 
-    public void requestBook(String title, String author, String publisher) {
+    // 희망도서 신청
+    public int requestBook(String title, String author, String publisher) {
+        int count = requestBookDao.confirmBook(title);
+
+        if (count > 0) {
+            System.out.println("❗ 이미 존재하는 도서입니다. 신청이 불가능합니다.");
+            return 0;
+        }
+
         Date today =  new Date(System.currentTimeMillis());
         RequestsDto dto = new RequestsDto();
         Long userId = 2L;
@@ -29,20 +38,24 @@ public class RequestBookService {
         } else {
             System.out.println("신청 실패, 다시 신청해주세요.");
         }
+
+        return requestBookDao.requestBook(dto);
     }
 
+    // 희망도서 신청 조회(회원)
     public List<RequestsDto> getMyRequestedBooks() {
         RequestsDto dto = new RequestsDto();
-        Long userId = 2L;
+        Long userId = 1L;
 
         return requestBookDao.getMyRequestedBooks(userId);
     }
 
+    // 희망도서 신청 조회(관리자)
     public List<RequestsDto> getAllRequestedBooks() {
-
         return requestBookDao.getAllRequestedBooks();
     }
 
+    // 희망도서 상태 처리
     public int updateRequestedBookStatus(int requestId, int status) {
         return requestBookDao.updateRequestedBookStatus(requestId, status);
     }
