d14940502c919a47113b1ab1ce2d96759ba62e03
Patrick <jjgus29@naver.com>
2025-04-30 16:19:46 +0900

    feat : 유저관리 기능 추가 - 유저 조회(모두 조회, id로 조회), 삭제, 업데이트

---
+++ src/main/java/model/dao/UserManageDao.java
@@ -0,0 +1,93 @@
+package model.dao;
+
+import model.dto.UserDto;
+import model.sql.UserManageSql;
+import util.DBUtil;
+import util.MapResultSetToDto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserManageDao {
+    // 모든 유저 목록 조회
+    public List<UserDto> getAllUsers() {
+        String sql = UserManageSql.SELECT_ALL_USER;
+        List<UserDto> users = new ArrayList<>();
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet rs = preparedStatement.executeQuery()) {
+
+            while (rs.next()) {
+                users.add(MapResultSetToDto.mapResultSetToUserDto(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
+    /**
+     * Select문이면서 parameter존재할 때 Connection 및 Statement이후 ResultSet은 별도로 처리
+     */
+    public List<UserDto> getUserById(Long userId) {
+        String sql = UserManageSql.SELECT_USER_BY_ID;
+        List<UserDto> users = new ArrayList<>();
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setLong(1, userId);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    users.add(MapResultSetToDto.mapResultSetToUserDto(rs));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    // 유저 정보 업데이트
+    public int updateUser(String name, String email, String phone_number, Long user_id) {
+        String sql = UserManageSql.UPDATE_USER;
+
+        try (Connection conn = DBUtil.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, name);
+            pstmt.setString(2, email);
+            pstmt.setString(3, phone_number);
+            pstmt.setLong(4, user_id);
+
+            return pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public int deleteUser(Long userId) {
+        String sql = UserManageSql.DELETE_USER;
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setLong(1, userId);
+
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+}
