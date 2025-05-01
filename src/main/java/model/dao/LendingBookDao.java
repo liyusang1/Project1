86225b5434b43ab64a52dc4d6f6544b221d0c781
liyusang1 <liyusang1@naver.com>
2025-05-01 09:49:57 +0900

    fix : 관리자는 연체납부 대상이 되지 않도록 코드 수정

--- src/main/java/model/dao/LendingBookDao.java
+++ src/main/java/model/dao/LendingBookDao.java
@@ -264,11 +264,8 @@ public class LendingBookDao {
         try (Connection connection = DBUtil.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
-            // 먼저 파라미터 설정
             preparedStatement.setLong(1, userId);
 
-            // 쿼리 실행
-            // 책은 최대 3개 까지 대출 가능 ->
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next() && resultSet.getInt("fee") >= 1) {
                     available = true;
