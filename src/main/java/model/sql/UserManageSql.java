926520b18a6bf0a76c6385983750565d51e5afa0
Patrick <jjgus29@naver.com>
2025-05-01 10:48:29 +0900

    feat : UserManage view 구현 - 회원 조회 세분화 (1.전체조회 2. 관리자조회 3. 일반회원 조회 4. 가입 기준 정렬), Table생성 helper util

--- src/main/java/model/sql/UserManageSql.java
+++ src/main/java/model/sql/UserManageSql.java
@@ -1,8 +1,17 @@
 package model.sql;
 
 public class UserManageSql {
-    // membership_type은 꼭 불러올 것
-    public static final String SELECT_ALL_USER = "SELECT user_id, name, email, membership_type FROM Users";
+    /**
+     * parameter없는 query
+     */
+    public static final String SELECT_ALL_USER = "SELECT * FROM Users";
+    public static final String SELECT_ALL_MANAGER = "SELECT * FROM Users WHERE membership_type = 1";
+    public static final String SELECT_ALL_NORMAL_USER = "SELECT * FROM Users WHERE membership_type = 0";
+    public static final String SELECT_ALL_USER_BY_CARETE_DATE = "SELECT * FROM Users ORDER BY created_at DESC";
+
+    /**
+     *  parameter 포함 query
+     */
     public static final String SELECT_USER_BY_ID = "SELECT * FROM Users WHERE user_id = ?";
     public static final String UPDATE_USER = "UPDATE Users SET name = ?, email = ?, phone_number = ? WHERE user_id = ?";
     public static final String DELETE_USER = "DELETE FROM Users WHERE user_id = ?";
