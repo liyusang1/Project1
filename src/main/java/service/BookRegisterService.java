80726b15b66784b88ad5378f09bdb2992444a9a5
Patrick <jjgus29@naver.com>
2025-05-02 12:27:04 +0900

    refactor : regex 상세내용 추가

--- src/main/java/service/BookRegisterService.java
+++ src/main/java/service/BookRegisterService.java
@@ -1,5 +1,6 @@
 package service;
 
+import constants.ResultCode;
 import model.dao.BookRegisterDao;
 import model.dao.CheckCateoryExistDao;
 import model.dao.CheckMangerDao;
@@ -38,46 +39,61 @@ public class BookRegisterService {
         // 관리자 유저인지 체크
         if (!checkMangerDao.checkManager(userId)) {
             System.out.println("[관리자만 접근 가능합니다]");
-            return 0;
+            return ResultCode.IS_FAIL;
         }
 
         // category_id 카테고리 테이블에 없는 경우 0 반환
         if (!checkCateoryExistDao.checkCategoryExist(bookDto.getCategoryId())) {
             System.out.print("[존재하지 않는 카테고리]");
-            return 0;
+            return ResultCode.IS_FAIL;
         }
 
-        // title 비어있거나 255글자 이상일 경우 0 반환
-        if (!bookDto.getTitle().matches("^.{0,255}$")) {
-            System.out.print("[책이름이 너무 깁니다]");
-            return 0;
-        } else if (bookDto.getTitle().trim().isEmpty()) {
-            System.out.print("[책이름 입력값이 없습니다]");
-            return 0;
+        // title: 1~100자, 한글/영문/숫자/공백/특수문자 허용
+        if (!bookDto.getTitle().matches("^.{1,100}$")) {
+            if (bookDto.getTitle().trim().isEmpty()) {
+                System.out.print("[책이름 입력값이 없습니다]");
+            } else if (bookDto.getTitle().length() > 100) {
+                System.out.print("[책이름이 너무 깁니다]");
+            } else {
+                System.out.print("[책이름 형식이 올바르지 않습니다]");
+            }
+            return ResultCode.IS_FAIL;
         }
 
-        //  author 255글자 이상일 경우 0 반환
-        if (!bookDto.getAuthor().matches("^.{0,255}$")) {
-            System.out.print("[저자이름이 너무 깁니다]");
-            return 0;
-        } else if (bookDto.getAuthor().trim().isEmpty()) {
-            System.out.print("[저자이름 입력값이 없습니다]");
-            return 0;
+        // author: 2~50자, 한글/영문/공백만 허용
+        if (!bookDto.getAuthor().matches("^[a-zA-Z가-힣\\s]{2,50}$")) {
+            if (bookDto.getAuthor().trim().isEmpty()) {
+                System.out.print("[저자이름 입력값이 없습니다]");
+            } else if (bookDto.getAuthor().length() > 50) {
+                System.out.print("[저자이름이 너무 깁니다]");
+            } else if (bookDto.getAuthor().length() < 2) {
+                System.out.print("[저자이름이 너무 짧습니다]");
+            } else {
+                System.out.print("[저자이름 형식이 올바르지 않습니다]");
+            }
+            return ResultCode.IS_FAIL;
         }
 
-        // publisher 비어있거나 255글자 이상일 경우 0 반환
-        if (!bookDto.getPublisher().matches("^.{0,255}$")) {
-            System.out.print("[출판사이름 너무 깁니다]");
-            return 0;
-        } else if (bookDto.getPublisher().trim().isEmpty()) {
-            System.out.print("[출판사이름 입력값이 없습니다]");
-            return 0;
+        // publisher: 1~50자, 한글/영문/숫자/공백/특수문자 허용
+        if (!bookDto.getPublisher().matches("^.{1,50}$")) {
+            if (bookDto.getPublisher().trim().isEmpty()) {
+                System.out.print("[출판사이름 입력값이 없습니다]");
+            } else if (bookDto.getPublisher().length() > 50) {
+                System.out.print("[출판사이름이 너무 깁니다]");
+            } else {
+                System.out.print("[출판사이름 형식이 올바르지 않습니다]");
+            }
+            return ResultCode.IS_FAIL;
         }
 
-        // price 입력값
-        if (bookDto.getPrice() > 100000000) {
-            System.out.print("[가격 입력값이 너무 큽니다]");
-            return 0;
+        // price: 1000만원 이하, 0보다 큰 값만 허용
+        if (bookDto.getPrice() > 10000000 || bookDto.getPrice() <= 0) {
+            if (bookDto.getPrice() > 10000000) {
+                System.out.print("[가격 입력값이 너무 큽니다]");
+            } else {
+                System.out.print("[가격 입력값이 0 이하입니다]");
+            }
+            return ResultCode.IS_FAIL;
         }
 
         return bookRegisterDao.registerBook(bookDto);
