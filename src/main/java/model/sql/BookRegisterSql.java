5292f31609e3be3e151b574b5a063df2b648cf3f
Patrick <jjgus29@naver.com>
2025-05-02 12:27:04 +0900

    refactor : regex 상세내용 추가

--- src/main/java/model/sql/BookRegisterSql.java
+++ src/main/java/model/sql/BookRegisterSql.java
@@ -8,14 +8,14 @@ public class BookRegisterSql {
 
     public static final String SELECT_ALL_BOOKS = "SELECT b.book_id, b.title, b.author, " +
         "b.category_id, c.category AS category, b.publisher, b.price, b.status, b.created_at " +
-        "FROM Books b INNER JOIN Categorys c ON b.category_id = c.category_id ORDER BY b.created_at DESC" ;
+        "FROM Books b INNER JOIN Categorys c ON b.category_id = c.category_id ORDER BY b.created_at DESC LIMIT 15" ;
 
 
     public static final String SELECT_AVAILABLE_BOOKS =
             "SELECT b.book_id, b.title, b.author, b.category_id, c.category AS category, " +
                     "b.publisher, b.price, b.status, b.created_at " +
                     "FROM Books b INNER JOIN Categorys c ON b.category_id = c.category_id " +
-                    "WHERE b.status = 1 ORDER BY b.created_at DESC";
+                    "WHERE b.status = 1 ORDER BY b.created_at DESC LIMIT 15";
 
     public static final String SELECT_BOOK_BY_ID = "SELECT b.book_id, b.title, b.author, " +
             "b.category_id, c.category AS category, b.publisher, b.price, b.status, b.created_at " +
